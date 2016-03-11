13e4e09c782045799df4b2b96acc9cb42673a0c0
Egis <e.gecius@gmail.com>
2016-03-11 10:24:56 +0000

    JavaDocs added

--- app/src/main/java/DataClasses/Artist.kt
+++ app/src/main/java/DataClasses/Artist.kt
@@ -1,5 +1,12 @@
 package DataClasses
 
+/**
+ * The following are generated for you:
+     * getters & setters
+     * toString
+     * hashCode
+     * equals
+ */
 data class Artist(
         var id: Long,
         var name: String,
