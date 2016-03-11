1fe12c4aa873af1ab425fb88d4c29b875c385028
Egis <e.gecius@gmail.com>
2016-03-11 12:27:44 +0000

    DetailActivity added

---
+++ app/src/main/java/com/egecius/demo_kotlin/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.egecius.demo_kotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.detail_activity)
+    }
+}
--- app/src/main/java/com/egecius/demo_kotlin/MainActivity.kt
+++ app/src/main/java/com/egecius/demo_kotlin/MainActivity.kt
@@ -2,8 +2,8 @@ package com.egecius.demo_kotlin
 
 import Functions.baseFunction
 import Functions.showToast
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.egecius.demo_kotlin.types.ChildType
 
 class MainActivity : AppCompatActivity() {
