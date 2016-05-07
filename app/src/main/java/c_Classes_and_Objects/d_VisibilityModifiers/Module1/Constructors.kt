47b09bfe929f3fcb867d7508741c00f0cc334a21
Egis <e.gecius@gmail.com>
2016-05-07 17:09:53 +0100

    primary constructor visibility

---
+++ app/src/main/java/c_Classes_and_Objects/d_VisibilityModifiers/Module1/Constructors.kt
@@ -0,0 +1,8 @@
+package c_Classes_and_Objects.d_VisibilityModifiers.Module1
+
+/**
+ * Shows how to set visibility of primary constructor
+ */
+internal class Constructors private constructor(val name: String) {
+
+}
