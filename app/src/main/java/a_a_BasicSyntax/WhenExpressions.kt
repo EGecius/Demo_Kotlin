9cca96b6aea18fc783c3c186e19ffe485936ed8e
Egis <e.gecius@gmail.com>
2016-05-07 09:20:11 +0100

    when statements

---
+++ app/src/main/java/a_a_BasicSyntax/WhenExpressions.kt
@@ -0,0 +1,66 @@
+package a_a_BasicSyntax
+
+/**
+ * 'when' replaces 'switch'
+ */
+internal class WhenExpressions {
+
+    fun asExpression(x: String): Boolean {
+
+        val hasPrefix = when (x) {
+            is String -> x.startsWith("prefix")
+            else -> false
+        }
+
+        return hasPrefix
+    }
+
+    fun asStatement(x: Int) : String {
+
+        val validNumbers = intArrayOf(3, 4)
+
+        val result : String
+
+        when (x) {
+            1, 2 -> result = ONE_OR_TWO
+            in validNumbers -> result = IN_VALID_NUMBERS
+            in 5..10 -> result = BETWEEN_5_AND_10
+            !in 11..20 -> result = OUTSIDE_RANGE
+            else -> result = SOMETHING_ELSE
+        }
+
+        return result
+    }
+
+    fun expressionAsBranchCondition(x: Int) : String {
+
+        val s = "five"
+
+        when (x) {
+            parseInt(s) -> return S_ENCODES_X
+            else -> return S_DOES_NOT_ENCODE_X
+        }
+    }
+
+    private fun parseInt(s: String): Int {
+        if (s.equals("five")) {
+            return 5
+        } else {
+            return -1
+        }
+    }
+
+
+    companion object {
+        val ONE_OR_TWO = "1 OR 2"
+        val IN_VALID_NUMBERS = "in valid numbers"
+        val BETWEEN_5_AND_10 = "BETWEEN_5_AND_10"
+        val SOMETHING_ELSE = "SOMETHING_ELSE"
+        val OUTSIDE_RANGE = "OUTSIDE_RANGE"
+        val S_ENCODES_X = "S_ENCODES_X"
+        val S_DOES_NOT_ENCODE_X = "S_DOES_NOT_ENCODE_X"
+    }
+
+}
+
+
---
+++ app/src/test/java/a_a_BasicSyntax/WhenExpressionsTest.kt
@@ -0,0 +1,78 @@
+package a_a_BasicSyntax
+
+import org.junit.Test
+
+/**
+ * Tests for [WhenExpressions]
+ */
+class WhenExpressionsTest {
+
+    internal var classUnderTest: WhenExpressions = WhenExpressions()
+
+    @Test
+    fun asExpression() {
+        //WHEN
+        val hasPrefix = classUnderTest.asExpression("prefix_Value");
+        //THEN
+        assert(hasPrefix)
+    }
+
+    @Test
+    fun asExpression2() {
+        //WHEN
+        val hasPrefix = classUnderTest.asExpression("Value");
+        //THEN
+        assert(!hasPrefix)
+    }
+
+
+    @Test
+    fun asStatement() {
+        //WHEN
+        val result = classUnderTest.asStatement(1);
+        //THEN
+        assert(result.equals(WhenExpressions.ONE_OR_TWO))
+    }
+
+
+    @Test
+    fun asStatement2() {
+        //WHEN
+        val result = classUnderTest.asStatement(3);
+        //THEN
+        assert(result.equals(WhenExpressions.IN_VALID_NUMBERS))
+    }
+
+    @Test
+    fun asStatement3() {
+        //WHEN
+        val result = classUnderTest.asStatement(5);
+        //THEN
+        assert(result.equals(WhenExpressions.BETWEEN_5_AND_10))
+    }
+
+    @Test
+    fun asStatement4() {
+        //WHEN
+        val result = classUnderTest.asStatement(11);
+        //THEN
+        assert(result.equals(WhenExpressions.SOMETHING_ELSE))
+    }
+
+    @Test
+    fun expressionAsBranchCondition() {
+        //WHEN
+        val result = classUnderTest.expressionAsBranchCondition(5);
+        //THEN
+        assert(result.equals(WhenExpressions.S_ENCODES_X))
+    }
+
+    @Test
+    fun expressionAsBranchCondition2() {
+        //WHEN
+        val result = classUnderTest.expressionAsBranchCondition(6);
+        //THEN
+        assert(result.equals(WhenExpressions.S_DOES_NOT_ENCODE_X))
+    }
+
+}
