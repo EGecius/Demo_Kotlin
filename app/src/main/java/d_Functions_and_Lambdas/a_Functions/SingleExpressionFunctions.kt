3df64311e1fa8d434bc1fb7392460846afd5cf53
Egis <e.gecius@gmail.com>
2016-05-13 08:16:34 +0100

    single expression functions

---
+++ app/src/main/java/d_Functions_and_Lambdas/a_Functions/SingleExpressionFunctions.kt
@@ -0,0 +1,7 @@
+package d_Functions_and_Lambdas.a_Functions
+
+// When a function returns a single expression, the curly braces can be omitted and the body is specified after a= symbol
+fun double(x: Int): Int = x * 2
+
+// Explicitly declaring the return type is optional when this can be inferred by the compiler
+fun double2(x: Int) = x * 2
