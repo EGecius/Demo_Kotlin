5f52766e785bb878d5eb5905ff805d9907fc82bb
Egidijus Gecius <e.gecius@gmail.com>
2017-04-25 17:22:31 +0100

    add constants

---
+++ app/src/main/java/b_Basics/c_ControlFlow/CompileTimeConstants.kt
@@ -0,0 +1,18 @@
+package b_Basics.c_ControlFlow
+
+
+
+const val MY_CONSTANT = "MY_CONSTANT"
+
+
+/** Constants are marked with 'const' and are only allowed in top-level or in objects, i.e. no in
+ classes *  */
+class CompileTimeConstants {
+
+    companion object {
+
+        const val MY_OTHER_CONSTANT = "MY_OTHER_CONSTANT"
+
+    }
+
+}
