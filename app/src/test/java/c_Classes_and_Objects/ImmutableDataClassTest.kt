59a273df5631ffb8597ebd75e79ed48bc585ab7d
Egidijus Gecius <e.gecius@gmail.com>
2017-04-28 10:40:08 +0100

    add components

--- app/src/test/java/c_Classes_and_Objects/ImmutableDataClassTest.kt
+++ app/src/test/java/c_Classes_and_Objects/ImmutableDataClassTest.kt
@@ -1,12 +1,20 @@
 package c_Classes_and_Objects
 
 import c_Classes_and_Objects.f_Data_Classes.ImmutableDataClass
-import org.junit.Test
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
 
 class ImmutableDataClassTest {
 
-    internal var immutable = ImmutableDataClass(1)
+
+    companion object {
+
+        const val DEFAULT_ID = 1
+        const val DEFAULT_NAME = "default name"
+
+    }
+
+    internal var immutable = ImmutableDataClass(DEFAULT_ID)
 
     @Test
     fun when_tryingToFindSetter_then_notFound() {
@@ -16,12 +24,18 @@ class ImmutableDataClassTest {
 
     @Test
     fun when_gettingName_then_defaultNameFound() {
-        assertThat(immutable.name).isEqualTo("default name")
+        assertThat(immutable.name).isEqualTo(DEFAULT_NAME)
     }
 
     @Test
     fun when_gettingId_then_setIdFound() {
-        assertThat(immutable.id).isEqualTo(1)
+        assertThat(immutable.id).isEqualTo(DEFAULT_ID)
+    }
+
+    @Test
+    fun componentsCorrespondToProperties() {
+        assertThat(immutable.component1()).isEqualTo(DEFAULT_ID)
+        assertThat(immutable.component2()).isEqualTo(DEFAULT_NAME)
     }
 
 }
