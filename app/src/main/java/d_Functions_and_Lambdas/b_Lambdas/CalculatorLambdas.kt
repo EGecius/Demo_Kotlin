bdfbe0dfc85ab4a54f4df6f8ca7b439da790013d
Egis <e.gecius@gmail.com>
2017-06-08 07:45:04 +0100

    make lambdas example more readable

--- app/src/main/java/d_Functions_and_Lambdas/b_Lambdas/CalculatorLambdas.kt
+++ app/src/main/java/d_Functions_and_Lambdas/b_Lambdas/CalculatorLambdas.kt
@@ -1,16 +1,15 @@
 package d_Functions_and_Lambdas.b_Lambdas
 
 /**
- * Shows hot to pass lambdas to build a calculator
+ * Shows how to pass lambdas to build a calculator
  */
-
-internal fun operation(x: Int, y: Int, operation: (Int, Int) -> Int) = operation(x, y)
 
 fun add(x: Int, y: Int) = operation(x, y, sum)
 fun deduct(x: Int, y: Int) = operation(x, y, deduct)
 fun multiply(x: Int, y: Int) = operation(x, y, multiply)
 fun divide(x: Int, y: Int) = operation(x, y, divide)
 
+internal fun operation(x: Int, y: Int, operationLambda: (Int, Int) -> Int) = operationLambda(x, y)
 
 val sum = { x: Int, y: Int -> x + y }
 
