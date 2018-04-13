9de9f2924d6a4a5a4b4ca70e26c392ee60f8631f
Egidijus Gecius <e.gecius@gmail.com>
2018-04-13 15:02:45 +0100

    add sample with indices

--- app/src/test/java/a_GettingStarted/a_BasicSyntax/LoopsTest.kt
+++ app/src/test/java/a_GettingStarted/a_BasicSyntax/LoopsTest.kt
@@ -1,5 +1,6 @@
 package a_GettingStarted.a_BasicSyntax
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
 /**
@@ -30,6 +31,23 @@ class LoopsTest {
         val sum = loops.sumWithForiLoopReplacement(4)
         //THEN
         assert(sum == 10)
+    }
+
+    @Test
+    fun loopWitIndices() {
+        val modifiedItems = mutableListOf<String>()
+
+        val items = listOf("apple", "banana", "kiwifruit")
+        for (index in items.indices) {
+            val newString = "item at $index is ${items[index]}"
+            modifiedItems.add(newString)
+        }
+
+        val expectedList = listOf(
+                "item at 0 is apple",
+                "item at 1 is banana",
+                "item at 2 is kiwifruit")
+        assertThat(modifiedItems).isEqualTo(expectedList)
     }
 
 }
