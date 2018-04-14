d564395a702f23b3fd8d1191499708da2b8af655
Egidijus Gecius <e.gecius@gmail.com>
2018-04-14 09:15:07 +0100

    WithIdiom

---
+++ app/src/main/java/a_GettingStarted/b_idioms/WithIdiom.kt
@@ -0,0 +1,25 @@
+package a_GettingStarted.b_idioms
+
+class WithIdiom {
+
+    fun showWithIdiom() {
+
+        val myTurtle = Turtle()
+        with(myTurtle) {
+            //draw a 100 pix square
+            penDown()
+            for (i in 1..4) {
+                forward(100.0)
+                turn(90.0)
+            }
+            penUp()
+        }
+    }
+}
+
+class Turtle {
+    fun penDown() {}
+    fun penUp() {}
+    fun turn(degrees: Double) {}
+    fun forward(pixels: Double) {}
+}
