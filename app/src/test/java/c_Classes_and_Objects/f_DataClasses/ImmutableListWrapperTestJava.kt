cb2b07879908afe110ac3208355a6b4a8ce756b8
Egidijus Gecius <e.gecius@gmail.com>
2018-04-26 16:42:20 +0100

    simplify ImmutableListWrapperTestJava

--- app/src/test/java/c_Classes_and_Objects/f_DataClasses/ImmutableListWrapperTestJava.kt
+++ app/src/test/java/c_Classes_and_Objects/f_DataClasses/ImmutableListWrapperTestJava.kt
@@ -1,19 +1,11 @@
 package c_Classes_and_Objects.f_DataClasses
 
-import c_Classes_and_Objects.f_Data_Classes.ImmutableListWrapper
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.Before
 import org.junit.Test
 
 class ImmutableListWrapperTestJava {
 
     private var injectedList = mutableListOf(ORIGINAL_VALUE)
-    private lateinit var wrapper: ImmutableListWrapper
-
-    @Before
-    fun setup() {
-        wrapper = ImmutableListWrapper(injectedList)
-    }
 
     @Test
     fun when_then() {
