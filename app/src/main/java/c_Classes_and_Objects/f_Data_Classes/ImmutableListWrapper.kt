43dfac358f02d40f8589e38b616a54f640a5a893
Egidijus Gecius <e.gecius@gmail.com>
2018-04-26 16:41:05 +0100

    simplify ImmutableListWrapperTestJava

--- app/src/main/java/c_Classes_and_Objects/f_Data_Classes/ImmutableListWrapper.kt
+++ app/src/main/java/c_Classes_and_Objects/f_Data_Classes/ImmutableListWrapper.kt
@@ -1,5 +1,4 @@
 package c_Classes_and_Objects.f_Data_Classes
 
 
-data class ImmutableListWrapper(val list: MutableList<String>) {
-}
+data class ImmutableListWrapper(val list: MutableList<String>)
--- app/src/test/java/c_Classes_and_Objects/f_DataClasses/ImmutableListWrapperTestJava.kt
+++ app/src/test/java/c_Classes_and_Objects/f_DataClasses/ImmutableListWrapperTestJava.kt
@@ -4,38 +4,27 @@ import c_Classes_and_Objects.f_Data_Classes.ImmutableListWrapper
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
-import java.util.*
 
-/**
- * For som reason tests do not run in this class. Is it because I am trying to test Kotlin class in Java?
- */
-@RunWith(JUnit4::class)
 class ImmutableListWrapperTestJava {
-    private lateinit var wrapper: ImmutableListWrapper
-    private var injectedList: ArrayList<String>? = null
 
+    private var injectedList = mutableListOf(ORIGINAL_VALUE)
+    private lateinit var wrapper: ImmutableListWrapper
 
     @Before
     fun setup() {
-        injectedList = ArrayList()
-        injectedList!!.add(ORIGINAL_VALUE)
-        wrapper = ImmutableListWrapper(injectedList!!)
+        wrapper = ImmutableListWrapper(injectedList)
     }
 
     @Test
     fun when_then() {
-        //WHEN
-        val valueFound = injectedList!![0]
-        //THEN
+        val valueFound = injectedList[0]
+
         assertThat(valueFound).isEqualTo(ORIGINAL_VALUE)
     }
 
     companion object {
 
-        val ORIGINAL_VALUE = "original value"
+        const val ORIGINAL_VALUE = "original value"
     }
-
 
 }
