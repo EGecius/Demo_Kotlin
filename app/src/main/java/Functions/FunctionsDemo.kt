d69b476ee3b5939a87f4bbfffbafe4a0ace718e3
Egidijus Gecius <e.gecius@gmail.com>
2018-04-26 08:37:29 +0100

    demo passing lambdas

---
+++ app/src/main/java/Functions/FunctionsDemo.kt
@@ -0,0 +1,6 @@
+package Functions
+
+
+
+class FunctionsDemo
+
---
+++ app/src/test/java/Functions/FunctionsDemoTest.kt
@@ -0,0 +1,35 @@
+package Functions
+
+import org.junit.Before
+import org.junit.Test
+
+class FunctionsDemoTest {
+
+    private var mSut: FunctionsDemo? = null
+
+    @Before
+    fun setUp() {
+        mSut = FunctionsDemo()
+    }
+
+    fun foo(bar: Int = 0, baz: Int = 1, qux: () -> Unit) {
+        qux()
+    }
+
+    fun execute() {
+        println("hello 3")
+    }
+
+    fun execute(myLambda: () -> Unit) {
+        myLambda()
+    }
+
+    @Test
+    fun `demo passing lambdas to method`() {
+
+        execute { println("hello 3") }
+
+        foo { println("hello 4") }
+    }
+
+}
