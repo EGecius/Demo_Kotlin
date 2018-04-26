69497fae780fbf16e2d8822f11e8ce9ae78cac33
Egidijus Gecius <e.gecius@gmail.com>
2018-04-26 16:53:42 +0100

    fix ContainerTest_InjectMocks

--- app/src/test/java/y_mocking/ContainerTest_InjectMocks.kt
+++ app/src/test/java/y_mocking/ContainerTest_InjectMocks.kt
@@ -1,6 +1,7 @@
 package y_mocking
 
 import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.eq
 import com.nhaarman.mockito_kotlin.verify
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -11,16 +12,16 @@ import org.mockito.junit.MockitoJUnitRunner
 @RunWith(MockitoJUnitRunner::class)
 class ContainerTest_InjectMocks {
 
-    @Mock lateinit var dependency : Dependency
-
-    @InjectMocks lateinit var container : Container
+    @Mock
+    lateinit var dependency: Dependency
+    @InjectMocks
+    lateinit var container: Container
 
     @Test
     fun mockitoWorks() {
-        //WHEN
         container.doTheThing(4)
-        //THEN
-        verify(dependency).alsoDoSomething(4, any())
+
+        verify(dependency).alsoDoSomething(eq(4), any())
     }
 
 }
