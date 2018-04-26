34fe486ae2596103d0f93a9c548806a77b80f469
Egidijus Gecius <e.gecius@gmail.com>
2018-04-26 16:36:55 +0100

    simplify ImmutableListWrapper_UsageTest

--- app/src/test/java/c_Classes_and_Objects/f_Data_Classes/ImmutableListWrapper_UsageTest.kt
+++ app/src/test/java/c_Classes_and_Objects/f_Data_Classes/ImmutableListWrapper_UsageTest.kt
@@ -1,27 +1,16 @@
 package c_Classes_and_Objects.f_Data_Classes
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.Before
 import org.junit.Test
-import java.util.*
 
 class ImmutableListWrapper_UsageTest {
 
-    private lateinit var sample: ImmutableListWrapper_Usage
-    private lateinit var list: ArrayList<String>
-
-    @Before
-    fun setup() {
-        list = ArrayList()
-        list.add(ORIGINAL_VALUE)
-        sample = ImmutableListWrapper_Usage()
-    }
+    private var sample = ImmutableListWrapper_Usage()
+    private var list = arrayListOf(ORIGINAL_VALUE)
 
     @Test
     fun when_listRetrieved_then_onlyAddedValuesFound() {
-        //WHEN
         val (list) = sample.getImmutableListWrapper(list)
-        //THEN
 
         assertThat(list.size).isEqualTo(1)
         assertThat(list[0]).isEqualTo(ORIGINAL_VALUE)
@@ -30,10 +19,9 @@ class ImmutableListWrapper_UsageTest {
     /** Shows that Kotlin's data class can be amended  */
     @Test
     fun when_listRetrieved_and_amended_then_originalIsAmended() {
-        //WHEN
         val immutableListWrapper = sample.getImmutableListWrapper(list)
         val listRetrieved = immutableListWrapper.list
-        //THEN
+
         assertThat(listRetrieved.size).isEqualTo(1)
         listRetrieved.clear()
 
@@ -43,7 +31,7 @@ class ImmutableListWrapper_UsageTest {
 
     companion object {
 
-        private val ORIGINAL_VALUE = "original value"
+        private const val ORIGINAL_VALUE = "original value"
     }
 
 }
