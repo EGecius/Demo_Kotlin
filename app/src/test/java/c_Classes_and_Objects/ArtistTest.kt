21400432f8a0881e1497113f748a87b4592fabf5
Egidijus Gecius <e.gecius@gmail.com>
2018-04-26 16:46:41 +0100

    simplify ArtistTest

--- app/src/test/java/c_Classes_and_Objects/ArtistTest.kt
+++ app/src/test/java/c_Classes_and_Objects/ArtistTest.kt
@@ -2,30 +2,24 @@ package c_Classes_and_Objects
 
 import c_Classes_and_Objects.f_Data_Classes.Artist
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.Before
 import org.junit.Test
 
 class ArtistTest {
 
-    private var artist: Artist? = null
-
-    @Before
-    fun setup() {
-        artist = Artist(ID.toLong(), "name", "url", "mbid")
-    }
+    private var artist = Artist(ID.toLong(), "name", "url", "mbid")
 
     @Test
     fun when_instantiated_thenSettersStillWork() {
-        assertThat(artist!!.id).isEqualTo(ID.toLong())
+        assertThat(artist.id).isEqualTo(ID.toLong())
 
-        artist!!.id = NEW_ID
+        artist.id = NEW_ID
 
-        assertThat(artist!!.id).isEqualTo(NEW_ID)
+        assertThat(artist.id).isEqualTo(NEW_ID)
     }
 
     companion object {
 
-        private val ID = 1
-        private val NEW_ID : Long = 2
+        private const val ID = 1
+        private const val NEW_ID : Long = 2
     }
 }
