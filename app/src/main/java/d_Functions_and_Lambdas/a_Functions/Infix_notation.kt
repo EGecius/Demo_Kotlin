83d43e3daaf2641312f23429c48cee29dd8a1990
Egidijus Gecius <e.gecius@gmail.com>
2018-05-01 11:30:27 +0100

    rename infix notation function

--- app/src/main/java/d_Functions_and_Lambdas/a_Functions/Infix_notation.kt
+++ app/src/main/java/d_Functions_and_Lambdas/a_Functions/Infix_notation.kt
@@ -1,15 +1,15 @@
 package d_Functions_and_Lambdas.a_Functions
 
 //define extension to nt
-infix fun Int.egis(x: Int): Int {
-    return 3
+infix fun Int.plus(addition: Int): Int {
+    return this + addition
 }
 
 fun useInfixFunction() {
     // call extension function using infix notation
-    1 egis  2
+    1 plus 2
 
     // is the same as
-    1.egis(2)
+    1.plus(2)
 
 }
