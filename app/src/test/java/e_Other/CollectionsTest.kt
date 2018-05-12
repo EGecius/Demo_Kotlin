1b93c6539503f82ca7d24d87119dfbc0216182d2
Egidijus Gecius <e.gecius@gmail.com>
2018-05-12 08:22:21 +0100

    dmeo requireNoNulls

--- app/src/test/java/e_Other/CollectionsTest.kt
+++ app/src/test/java/e_Other/CollectionsTest.kt
@@ -1,3 +1,5 @@
+@file:Suppress("UNUSED_VARIABLE")
+
 package e_Other
 
 import org.assertj.core.api.Assertions.assertThat
@@ -24,5 +26,24 @@ class CollectionsTest {
 
         assertThat(readWriteMap.get("foo")).isEqualTo(1)
         assertThat(readWriteMap["bar"]).isEqualTo(2)
+    }
+
+    @Test (expected = IllegalArgumentException::class)
+    fun `requireNoNulls throws IllegalArgumentException`() {
+
+        val listWithNull = listOf("one", null, "two")
+        assertThat(listWithNull[1]).isNull()
+
+        val listWithoutNulls = listWithNull.requireNoNulls()
+    }
+
+    @Test
+    fun `requireNoNulls returns same list when no nulls found`() {
+
+        val listWithNull = listOf("one", "two")
+
+        val listWithoutNulls = listWithNull.requireNoNulls()
+        assertThat(listWithoutNulls[0]).isEqualTo("one")
+        assertThat(listWithoutNulls[1]).isEqualTo("two")
     }
 }
