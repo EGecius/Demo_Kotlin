1a6116da48737b6f0b1c0b8d138560ab9e2825b7
Egidijus Gecius <e.gecius@gmail.com>
2018-05-12 10:01:08 +0100

    demo this with labels

---
+++ app/src/test/java/e_Other/ThisExpressionTest.kt
@@ -0,0 +1,35 @@
+package e_Other
+
+import org.junit.Test
+
+class ThisExpressionTest {
+
+    @Test
+    fun `demo this with labels`() {
+
+        class A { // implicit label @A
+
+            inner class B { // implicit label @B
+
+                fun Int.foo() { // implicit label @foo
+                    val a = this@A // A's this
+                    val b = this@B // B's this
+
+                    val c = this // foo()'s receiver, an Int
+                    val c1 = this@foo // foo()'s receiver, an Int
+
+                    val funLit = lambda@ fun String.() {
+                        val d = this // funLit's receiver
+                    }
+
+                    val funLit2 = { s: String ->
+                        // foo()'s receiver, since enclosing lambda expression
+                        // doesn't have any receiver
+                        val d1 = this
+                    }
+                }
+            }
+        }
+
+    }
+}
