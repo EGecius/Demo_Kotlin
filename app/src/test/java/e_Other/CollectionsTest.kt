88684a288b7e0cf98993154767d811d128fa6249
Egidijus Gecius <e.gecius@gmail.com>
2018-05-12 08:09:24 +0100

    `create snapshot of list`

--- app/src/test/java/e_Other/CollectionsTest.kt
+++ app/src/test/java/e_Other/CollectionsTest.kt
@@ -6,6 +6,19 @@ import org.junit.Test
 class CollectionsTest {
 
     @Test
+    fun `create snapshot of list`() {
+        val items = mutableListOf("one", "two")
+        val snapshotList = items.toList()
+
+        assertThat(snapshotList.size).isEqualTo(2)
+        assertThat(snapshotList[0]).isEqualTo("one")
+        assertThat(snapshotList[1]).isEqualTo("two")
+
+        items.add("three")
+//      snapshotList.add("three") //does not compile
+    }
+
+    @Test
     fun `demo hashMap creation`() {
         val readWriteMap = hashMapOf("foo" to 1, "bar" to 2)
 
