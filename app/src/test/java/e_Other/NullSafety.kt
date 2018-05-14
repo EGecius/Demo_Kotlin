9d3481877763e0e05513f82c61a86b2ec018deae
Egidijus Gecius <e.gecius@gmail.com>
2018-05-14 08:52:42 +0100

    `demo filterNotNull()`

--- app/src/test/java/e_Other/NullSafety.kt
+++ app/src/test/java/e_Other/NullSafety.kt
@@ -1,5 +1,6 @@
 package e_Other
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
 class NullSafety {
@@ -20,6 +21,14 @@ class NullSafety {
 
         val myClass = MyClass()
         myClass.let { println(it) }
+    }
+
+    @Test
+    fun `demo filterNotNull()`() {
+        val nullableList: List<Int?> = listOf(1, 2, null, 4)
+        val intList: List<Int> = nullableList.filterNotNull()
+
+        assertThat(intList).containsExactly(1, 2, 4)
     }
 
 }
