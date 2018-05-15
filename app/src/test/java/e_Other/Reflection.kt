03f0762271e853d7efaa7b140fc9e2d47b90d2b1
Egidijus Gecius <e.gecius@gmail.com>
2018-05-15 09:26:36 +0100

    Function References

--- app/src/test/java/e_Other/Reflection.kt
+++ app/src/test/java/e_Other/Reflection.kt
@@ -1,3 +1,5 @@
+@file:Suppress("UNUSED_VARIABLE")
+
 package e_Other
 
 import c_Classes_and_Objects.a_ClassesAndInheritance.MyClass
@@ -13,6 +15,17 @@ class Reflection {
         val javaReference = kotlinReference.java
 
         assertThat(kotlinReference).isNotEqualTo(javaReference)
+    }
+
+    @Test
+    fun `demo Function References`() {
+
+        fun isOdd(x: Int) = x % 2 != 0
+
+        val numbers = listOf(1, 2, 3)
+        val filtered = numbers.filter(::isOdd)
+
+        assertThat(filtered).containsExactly(1, 3)
     }
 
 }
