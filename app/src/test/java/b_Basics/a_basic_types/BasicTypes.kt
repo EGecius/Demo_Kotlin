f0e3fd2da525e9747c282ea01996e9f68e7ccff2
Egidijus Gecius <>
2018-05-25 15:38:02 +0100

    add samples of both lowercase & highercase

--- app/src/test/java/b_Basics/a_basic_types/BasicTypes.kt
+++ app/src/test/java/b_Basics/a_basic_types/BasicTypes.kt
@@ -2,6 +2,14 @@ package b_Basics.a_basic_types
 
 class Characters {
 
+    //Long with lowercase does not compile
+    //    val myLong2: Long = 123l
+    val myLong: Long = 123L
+
+    //Float compiles both lowercase and uppercase
+    val myFloat2: Float = 123.5f
+    val myFloat: Float = 123.5F
+
     fun checkCharDoesNotCompareToIn(c: Char) {
 //        if (c == 1) { // does not compiler
 //
