b4acdbba22e51c04969987e9840ae5d770141404
Egidjus Gecius <e.gecius@gmail.com>
2019-08-20 21:21:29 +0100

    demo how to use Kotlin's serialization library

--- app/src/main/java/z_android/DetailActivity.kt
+++ app/src/main/java/z_android/DetailActivity.kt
@@ -1,13 +1,23 @@
 package z_android
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.egecius.android.R
+import java.io.Serializable
 
 class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.detail_activity)
+    }
+
+    companion object {
+        fun openFrom(originActivity: Activity, extra: Serializable) {
+            val intent = Intent(originActivity, DetailActivity::class.java)
+            originActivity.startActivity(intent)
+        }
     }
 }
--- app/src/main/java/z_android/MainActivity.kt
+++ app/src/main/java/z_android/MainActivity.kt
@@ -1,11 +1,14 @@
 package z_android
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import c_Classes_and_Objects.e_Extensions.doubleTheString
 import c_Classes_and_Objects.e_Extensions.showToast
 import com.egecius.android.R
+import kotlinx.serialization.UnstableDefault
+import kotlinx.serialization.json.Json
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,6 +24,19 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.button).setOnClickListener {
             showToast("shown from extension function")
         }
+    }
 
+    override fun onStart() {
+        super.onStart()
+        demoSerialization()
+    }
+
+    @UseExperimental(UnstableDefault::class)
+    private fun demoSerialization() {
+        val person = Person("egis", "blue")
+
+        val personAsString: String = Json.stringify(Person.serializer(), person)
+
+        Log.i("Eg:MainActivity:32", "demoSerialization personAsString: $personAsString")
     }
 }
