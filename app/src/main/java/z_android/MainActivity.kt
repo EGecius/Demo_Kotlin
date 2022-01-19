1f3efc37fd43b759671801507cbc26e8547a08fc
Egidijus Gecius <e.gecius@gmail.com>
2022-01-19 09:41:28 +0000

    remove serialization dependency

--- app/src/main/java/z_android/MainActivity.kt
+++ app/src/main/java/z_android/MainActivity.kt
@@ -7,8 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 import c_Classes_and_Objects.e_Extensions.doubleTheString
 import c_Classes_and_Objects.e_Extensions.showToast
 import com.egecius.android.R
-import kotlinx.serialization.UnstableDefault
-import kotlinx.serialization.json.Json
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,19 +22,5 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.button).setOnClickListener {
             showToast("shown from extension function")
         }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        demoSerialization()
-    }
-
-    @UseExperimental(UnstableDefault::class)
-    private fun demoSerialization() {
-        val person = Person("egis", "blue")
-
-        val personAsString: String = Json.stringify(Person.serializer(), person)
-
-        Log.i("Eg:MainActivity:32", "demoSerialization personAsString: $personAsString")
     }
 }
--- app/src/main/java/z_android/Person.kt
+++
@@ -1,6 +0,0 @@
-package z_android
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class Person(val name: String, val favouriteColour: String)
